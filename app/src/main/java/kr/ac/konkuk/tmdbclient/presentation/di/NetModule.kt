1161e7fe02b85dc5b2e552a003b055a70004f39b
easyhooon <51016231+easyhooon@users.noreply.github.com>
2021-09-07 02:07:31 +0900

    di commit first

---
+++ app/src/main/java/kr/ac/konkuk/tmdbclient/presentation/di/NetModule.kt
@@ -0,0 +1,30 @@
+package kr.ac.konkuk.tmdbclient.presentation.di
+
+import dagger.Module
+import dagger.Provides
+import kr.ac.konkuk.tmdbclient.data.api.TMDBService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+// This module provides two dependencies . Retrofit instance and TMDB Service instance
+
+@Module
+class NetModule(private val baseUrl: String) {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTMDBService(retrofit: Retrofit):TMDBService {
+        return retrofit.create(TMDBService::class.java)
+    }
+
+}
---
+++ app/src/main/java/kr/ac/konkuk/tmdbclient/presentation/di/RemoteDataModule.kt
@@ -0,0 +1,7 @@
+package kr.ac.konkuk.tmdbclient.presentation.di
+
+import dagger.Module
+
+@Module
+class RemoteDataModule {
+}
